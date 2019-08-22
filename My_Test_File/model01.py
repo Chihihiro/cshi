2274b4a17dd3abe68eae0665e95c07ca24ffa1b9
chihiro <632207812@qq.com>
2019-08-22 15:35:01 +0800

    小阿三的

--- My_Test_File/model01.py
+++ My_Test_File/model01.py
@@ -21,7 +21,8 @@ class my_test(My_Tests):
             self.wait_xpath('//android.widget.TextView[@text="悦会会员"]').click()
             self.find_text_touch(text='我的会员', right=70, find="迪士尼及景区")
             self.wait_xpath('//android.widget.TextView[@text="迪士尼及景区"]').click()
-            self.wait_xpath('//android.widget.TextView[@text="全部"]').click()
+            # self.wait_xpath('//android.widget.TextView[@resource-id="com.yhouse.code:id/v_fake"]').click()
+            # self.wait_xpath('//android.widget.TextView[@text="全部"]').click()
             self.wait_xpath('//android.widget.TextView[@text="迪士尼乐园一日成人票"]').click()
             self.wait_xpath('//*[@content-desc="特惠购买"]').click()
             time.sleep(5)#需要多等待一会可能请求太快而失败
@@ -30,8 +31,13 @@ class my_test(My_Tests):
             info = Selector(text=html)
             ll = info.xpath('//@content-desc').extract()
             index_num = ll.index('今天')
-            text = ll[index_num + 1]
-            self.wait_xpath(f'//*[@content-desc="{text}"]').click()
+            text = ll[index_num + 2: index_num + 10]
+            tt = []
+            for i in text:
+                if '¥' in i:
+                    tt.append(i)
+            print(tt[3], '*'*30)
+            self.wait_xpath(f'//*[@content-desc="{tt[3]}"]').click()
             time.sleep(5)
             #填写信息
             name = self.wait_xpath('//android.widget.EditText[@text="请输入姓名"]')
