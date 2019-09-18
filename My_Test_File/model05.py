9d51351f27d9fd381f07df80914423617e63edd4
chihiro <632207812@qq.com>
2019-09-18 09:30:42 +0800

    小阿三的

--- My_Test_File/model05.py
+++ My_Test_File/model05.py
@@ -38,22 +38,27 @@ class my_test(My_Tests):
         # self.kill_advert()
         # try:
         self.wait_xpath('//android.widget.TextView[@text="悦会会员"]').click()
-        self.shanghai_city()
+        # self.shanghai_city()
         self.wait_xpath('//android.widget.TextView[@text="酒店特权"]').click()
-        # self.wait_xpath('//android.widget.ImageView[@resource-id="com.yhouse.code:id/ivHotel"]')[1].click()
-        self.wait_xpath('//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]/android.widget.RelativeLayout[1]/android.support.v4.view.ViewPager[1]/android.widget.RelativeLayout[1]/android.support.v7.widget.RecyclerView[1]/android.widget.FrameLayout[2]/android.widget.ImageView[1]').click()
-        num = random.randint(300, 500)
-        print('随机下滑', num)
-        time.sleep(10)
-        self.my_touch(xpath='//*[@content-desc="全部"]', down=-num)
+        # self.wait_xpath('//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]/android.widget.RelativeLayout[1]/android.support.v4.view.ViewPager[1]/android.widget.RelativeLayout[1]/android.support.v7.widget.RecyclerView[1]/android.view.ViewGroup[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.TextView[5]').click()
+        self.wait_xpath('//android.widget.TextView[@resource-id="com.yhouse.code:id/tv_findHotel"]').click()
+        self.wait_xpath('//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.support.v7.widget.RecyclerView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]').click()
+        # self.wait_xpath('//android.widget.TextView[@text="立即预订"]')[1].click()
+        # self.wait_xpath('//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]/android.widget.RelativeLayout[1]/android.support.v4.view.ViewPager[1]/android.widget.RelativeLayout[1]/android.support.v7.widget.RecyclerView[1]/android.widget.FrameLayout[2]/android.widget.ImageView[1]').click()
+
+        # num = random.randint(300, 500)
+        # print('随机下滑', num)
+
+        # time.sleep(10)
+        # self.my_touch(xpath='//*[@content-desc="全部"]', down=-num)
         # self.my_touch(xpath='//android.widget.TextView[@resource-id="com.yhouse.code:id/title_tv"]',
         #               down=-random.randint(300, 2000))
         # self.my_touch(xpath='//android.widget.TextView[@resource-id="com.yhouse.code:id/title_tv"]', down=-random.randint(300, 1000))
         # self.driver.find_element_by_xpath('//android.widget.TextView[@resource-id="com.yhouse.code:id/title_tv"]').click()
-        nn = random.randint(1, 4)
-        print(nn)
-        self.driver.find_element_by_xpath(f'//android.widget.ListView/android.view.View[{nn}]/android.view.View[2]').click()
-        # self.wait_xpath('//*[@content-desc="选择日期："]/following-sibling::android.view.View').click()
+        # nn = random.randint(1, 4)
+        # print(nn)
+        # self.driver.find_element_by_xpath(f'//android.widget.ListView/android.view.View[{nn}]/android.view.View[2]').click()
+        self.wait_xpath('//*[@content-desc="选择日期："]/following-sibling::android.view.View').click()
 
 
 
